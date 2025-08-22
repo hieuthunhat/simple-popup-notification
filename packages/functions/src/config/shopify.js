9ecf46e5e37b237dd497a5a31694bdf19ac7579e
hieunth <hieunguyentrong520@gmail.com>
2025-08-22 14:38:43 +0700

    update(permissions): update permissions for app

--- packages/functions/src/config/shopify.js
+++ packages/functions/src/config/shopify.js
@@ -6,6 +6,16 @@ export default {
   secret: shopify.secret,
   apiKey: shopify.api_key,
   firebaseApiKey: shopify.firebase_api_key,
-  scopes: shopify.scopes?.split(',') || ['read_themes'],
+  scopes: shopify.scopes?.split(',') || [
+    'read_products',
+    'write_orders',
+    'read_orders',
+    'write_script_tags',
+    'read_script_tags',
+    'write_products',
+    'write_themes',
+    'read_themes',
+    'read_customers'
+  ],
   accessTokenKey: shopify.access_token_key || 'avada-apps-access-token'
 };
