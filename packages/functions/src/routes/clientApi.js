2b196725be62c6cea975d742e9544cfa0d8d5d44
hieunth <hieunguyentrong520@gmail.com>
2025-08-29 00:22:31 +0700

    add(feat): add client route

---
+++ packages/functions/src/routes/clientApi.js
@@ -0,0 +1,11 @@
+import Router from 'koa-router';
+import * as clientController from '../controllers/clientControllers';
+const clientRouterApi = () => {
+  const router = new Router({prefix: '/clientApi'});
+
+  router.get('/notifications', clientController.getClientData);
+
+  return router;
+};
+
+export default clientRouterApi;
