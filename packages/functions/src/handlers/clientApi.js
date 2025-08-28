5e2a5abcb113b193e64323220462e1da5fec3e37
hieunth <hieunguyentrong520@gmail.com>
2025-08-29 00:29:16 +0700

    add(feat): add client handler

---
+++ packages/functions/src/handlers/clientApi.js
@@ -0,0 +1,16 @@
+import App from 'koa';
+import clientRouterApi from '@functions/routes/clientApi';
+
+const clientApi = new App();
+clientApi.proxy = true;
+
+const router = clientRouterApi();
+
+clientApi.use(router.allowedMethods());
+clientApi.use(router.routes());
+
+clientApi.on('error', (error, ctx) => {
+  console.error('Client API error:', error);
+});
+
+export default clientApi;
