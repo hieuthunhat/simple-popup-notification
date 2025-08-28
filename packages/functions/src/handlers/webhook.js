e6389bc628f471b34bfa44f4c5d38e8af13c79bd
hieunth <hieunguyentrong520@gmail.com>
2025-08-29 00:29:36 +0700

    add(feat): add webhook handler

--- packages/functions/src/handlers/webhook.js
+++ packages/functions/src/handlers/webhook.js
@@ -1,11 +1,13 @@
 import App from 'koa';
 import router from '../routes/webhook';
+import {verifyRequest} from '@avada/core';
 
 const webhookApi = new App();
 webhookApi.proxy = true;
 
 webhookApi.use(router.routes());
 webhookApi.use(router.allowedMethods());
+webhookApi.use(verifyRequest());
 
 webhookApi.on('error', (error, ctx) => {
   console.error('Webhook error:', error);
